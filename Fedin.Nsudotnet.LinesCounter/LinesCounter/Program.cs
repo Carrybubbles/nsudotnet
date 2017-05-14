767f963b63fc0cd6ff645bc693c1ad7ee461cea9
Carrybubbles <nick.dezlol@gmail.com>
2017-05-14 14:14:52 +0800

    fux names
    update

--- Fedin.Nsudotnet.LinesCounter/LinesCounter/Program.cs
+++ Fedin.Nsudotnet.LinesCounter/LinesCounter/Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LinesCounter
+namespace Fedin.Nsudotnet.LinesCounter
 {
     class Program
     {
