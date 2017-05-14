11758198f5a401e534f5a7aab661f241565d2133
Carrybubbles <nick.dezlol@gmail.com>
2017-05-14 14:03:49 +0800

    first commit
    my first C# labs

---
+++ LinesCounter/LinesCounter/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinesCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter path");
+            var path = Console.ReadLine();
+            
+            if (!string.IsNullOrEmpty(path))
+            {
+                var allFiles = Directory.GetFiles(path, args[0], SearchOption.AllDirectories);
+                var codeLines = 0;
+                foreach (var file in allFiles)
+                {
+                    using (var reader = new StreamReader(file))
+                    {
+                        string line;
+                        bool comment = false;
+                        while (null != (line = reader.ReadLine()))
+                        {
+                            line = line.Replace(" ", string.Empty);
+                            if (line.StartsWith("/*"))
+                            {
+                                comment = true;
+                            }
+                            else if (line.StartsWith("*/"))
+                            {
+                                comment = false;
+                            }
+                            else if (line.Equals("") | line.Equals("//"))
+                            {
+                            }
+                            else if (!comment)
+                            {
+                                codeLines++;
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine("Amount of Code lines in {0} = {1}", path, codeLines);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("cant open dir");             
+            }
+        }
+    }
+}
---
+++ NumberGuesser/NumberGuesser/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] humilations = {
+                "asshole", "idiot", "retard"
+            };
+            for(;;)
+            {
+                Console.WriteLine("Enter your name!");
+                var name = Console.ReadLine();
+                Console.WriteLine("Welcome {0}! To quit enter 'q' ", name);
+                Console.WriteLine("Enter your number, {0}", name);
+                var startTime = DateTime.Now;
+                var myNumber = new Random().Next(0, 100);
+                var tries = 1;
+                var history = new List<string>();
+                for (;;)
+                {
+                    var answer = Console.ReadLine();
+                    if (answer != null && answer.ToLower().Equals("q"))
+                    {
+                        Console.WriteLine("Sorry, my dear friend! Bye!");
+                        return;
+                    }
+
+                    int number;
+                    if (int.TryParse(answer, out number))
+                    {
+                        if (number == myNumber)
+                        {
+                            history.Add($"{number} == {myNumber}");
+                            Console.WriteLine("Congratulations! You guessed my number! ");
+                            Console.WriteLine("Your tries = {0}", tries);
+                            TimeSpan interval = DateTime.Now - startTime;
+                            Console.WriteLine("You lost {0} minutes", interval.Minutes);
+                            Console.WriteLine("Your history: ");
+                            history.ForEach(Console.WriteLine);                           
+                            Console.WriteLine("Do you want to continue? y/n ");
+                            for (;;)
+                            {
+                                var opt = Console.ReadLine();
+                                if (opt != null && opt.ToLower().Equals("y"))
+                                {
+                                    break;
+                                }
+                                if (opt != null && opt.ToLower().Equals("n"))
+                                {
+                                    Console.WriteLine("Bye");
+                                    return;
+                                }
+                                Console.WriteLine("What? Write again!");
+                            }
+                            break;
+                        }
+                        if (0 == tries % 4)
+                        {
+                            Console.WriteLine("You fucking {0}, {1}", humilations[new Random().Next() % 3], name);
+                        }
+                        if (number > myNumber)
+                        {
+                            Console.WriteLine("My number is less");
+                            history.Add($"{number} > {myNumber}");
+
+                        }
+                        else if (number < myNumber)
+                        {
+                            Console.WriteLine("My number is greater");
+                            history.Add($"{number} < {myNumber}");
+                        }
+                        tries++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("WTF?! Enter NUMBER! Try again");
+                    }
+                }
+            }
+        }
+    }
+}
